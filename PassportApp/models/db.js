2ee73d8d59b66b3a43b97aecee520115f99f26eb
Ashfaq Ali <mustafa.ash@hotmail.com>
2019-04-27 18:15:04 +0300

    db url changed

--- PassportApp/models/db.js
+++ PassportApp/models/db.js
@@ -2,18 +2,25 @@
 
 var gracefulShutdown;
 
-var dbURI = 'mongodb://birotydb:GvNlnWXftt5GubOdXDQ3vKf4AyWUG8ZTlFtrNB78tbJi7W4iHxmZTMFCR5gZX23sQpoLE1XYhBSP0Ns73NBhLA==@birotydb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'//'mongodb://localhost/bucketlist';
+var dbURI = "mongodb://dbadmin1:dbadmin1@cl1-shard-00-00-gz9xw.mongodb.net:27017,cl1-shard-00-01-gz9xw.mongodb.net:27017,cl1-shard-00-02-gz9xw.mongodb.net:27017/test?ssl=true&replicaSet=cl1-shard-0&authSource=admin&retryWrites=true";
+    //'mongodb+srv://dbadmin1:admin123@cl1-gz9xw.mongodb.net/db1';
+//'mongodb://birotydb@birotydb.documents.azure.com:10255/com_db?ssl=true&replicaSet=globaldb'
+//'mongodb://localhost/bucketlist';
 
 if (process.env.NODE_ENV === 'production') {
 
     dbURI = process.env.MONGOLAB_URI;
+    //split dburi to get user name and password for db
+    //mongoose.connect(dbURI, {
+    //    auth: {
+    //        user: 'birotydb',
+    //        password: 'GvNlnWXftt5GubOdXDQ3vKf4AyWUG8ZTlFtrNB78tbJi7W4iHxmZTMFCR5gZX23sQpoLE1XYhBSP0Ns73NBhLA=='
+    //    }
+    //});
 
 }
-
-
-
-mongoose.connect(dbURI);
-
+else
+    mongoose.connect(dbURI);
 
 
 // CONNECTION EVENTS
@@ -27,7 +34,6 @@ mongoose.connection.on('connected', function () {
 mongoose.connection.on('error', function (err) {
 
     console.log('Mongoose connection error: ' + err);
-
 });
 
 mongoose.connection.on('disconnected', function () {
